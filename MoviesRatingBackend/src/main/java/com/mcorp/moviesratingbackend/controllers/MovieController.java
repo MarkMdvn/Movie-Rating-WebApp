9a7135b317107e37e5b8cbebd05ee6654f26bbed
DaVinci0003 <133808076+DaVinci0003@users.noreply.github.com>
2024-05-26 02:54:38 +0200

    Finished the first version

--- MoviesRatingBackend/src/main/java/com/mcorp/moviesratingbackend/controllers/MovieController.java
+++ MoviesRatingBackend/src/main/java/com/mcorp/moviesratingbackend/controllers/MovieController.java
@@ -2,22 +2,17 @@ package com.mcorp.moviesratingbackend.controllers;
 
 import com.mcorp.moviesratingbackend.models.Movie;
 import com.mcorp.moviesratingbackend.services.MovieService;
-import org.apache.coyote.Response;
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
+@CrossOrigin(origins = "http://localhost:3000")
 public class MovieController {
 
     @Autowired
@@ -30,9 +25,7 @@ public class MovieController {
 
     @GetMapping("/{imdbId}")
 
-    public ResponseEntity<Optional<Movie>> getSingleMovie(
-            @PathVariable String imdbId)
-    {
+    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable String imdbId) {
         return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(imdbId), HttpStatus.OK);
     }
 }
